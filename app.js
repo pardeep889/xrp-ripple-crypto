8d26decaa9eefec06e1f2362a80c664891366199
pardeep889 <pardeep889@hotmail.com>
2019-02-19 15:06:07 +0530

    wallet Info DOne

--- app.js
+++ app.js
@@ -5,12 +5,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 var favicon = require('serve-favicon')
-var path = require('path')
-//private variables
+var path = require('path');
+var Wallet = require('./xrp/walletInfo');
 
-const api = new RippleAPI({
-    server: 'wss://s.altnet.rippletest.net:51233' // Public TEST rippled server
-  });
 
 //middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,24 +18,23 @@ app.use(favicon(path.join(__dirname, 'views', 'favicon.ico')))
 app.set('view engine', 'ejs');
 
 app.get('/', (req,res) => {
-    api.connect().then(() => {
-        const myAddress = process.env.XRPADDRESS ;      
-        console.log('getting account info for', myAddress);
-        return api.getAccountInfo(myAddress);
-      
-      }).then(info => {
-        console.log(info);
-        console.log('getAccountInfo done');
-        var data = {
-            xrp_address:process.env.XRPADDRESS ,
-            balance: info.xrpBalance
-          } 
-          res.render(__dirname + '/views/index', {data});
-      }).then(() => {
-        return api.disconnect();
-      }).then(() => {
-        console.log('done and disconnected.');
-      }).catch(console.error);    
+   res.send('Hello World');
+});
+
+app.get('/walletInfo', (req,res) => {
+  res.render(__dirname + '/views/wallet');
+})
+
+app.post('/walletInfo', (req,res) => {
+  // console.log(req.body.address);
+  let address = req.body.address.toString();
+  let walletInfoResolver = new Promise(function(resolve,reject){
+    Wallet.walletInfoXRP(resolve,address);
+  });
+  walletInfoResolver.then(function(data){
+      console.log(data);
+      res.render(__dirname + '/views/index', {data});
+  }); 
 });
 
 app.get('/payment', (req,res) => {
---
+++ xrp/walletInfo.js
@@ -0,0 +1,29 @@
+'use strict';
+const RippleAPI = require('ripple-lib').RippleAPI;
+
+const api = new RippleAPI({
+    server: process.env.server// Public TEST rippled server
+  });
+
+exports.walletInfoXRP = function walletInfoXRP(resolve,address){
+        // console.log(`${address},${secret}`);
+        api.connect().then(() => {
+        const myAddress = address ;      
+        console.log('getting account info for', myAddress);
+        return api.getAccountInfo(myAddress);      
+      }).then(info => {
+        // console.log(info);
+        console.log('getAccountInfo done');
+         var data = {
+            xrp_address:address ,
+            balance: info.xrpBalance
+          }        
+          if(data.balance){
+            resolve(data)
+          }  
+      }).then(() => {
+        return api.disconnect();
+      }).then(() => {
+        console.log('done and disconnected.');
+      }).catch(console.error);
+}
